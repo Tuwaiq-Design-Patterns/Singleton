b8cd9614a9416f5c6353125cc6910fccfa3c010b
Taghreed <Taghreed.Alqahtani@tuwaiq.edu.sa>
2021-06-01 22:53:03 +0300

    singleton project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Singleton
 this repo contains singleton design pattern implementation using C#
+
+المتدربة : تغريد القحطاني 
+
+المشرفة : نورة محسن 
+
+المجموعة : نيوم 
---
+++ singletonExample/singletonExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace singletonExample
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private Singleton() { }
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null) instance = new Singleton();
+                return instance;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton newInstance = Singleton.GetInstance;
+        }
+    }
+}
