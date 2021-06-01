8c405b82466c997faa14ec946eaea42f09021636
teefeid <taef_415@hotmail.com>
2021-06-01 21:55:37 +0300

    Singleton Concept

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Singleton
-this repo contains singleton design pattern implementation using C#
+# Singleton concept in simple word:
+A place that cannot be empty, such as the manager of the team, if he went, he would have to take the place of another manager immediately.
+
+#  Singleton example :
+* Airplane seat reservation
+
+* Director or head of state
+
+* Printer
+
+* Toolbar in the Mac system
+
+ # About Singleton:
+
+>* It is not executed unless requested by the client
+> * It doesn't change every time
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+
+    sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object locked = new Object(); //here we lock object if there is another object work 
+        private Singleton()
+        {
+        }
+        public static Singleton GetSingleton
+        {
+            get{
+               
+                {
+                    if (instance == null) //check if there is another instance of singleton or not . 
+                    {
+                        lock (locked)
+                        instance = new Singleton();
+                        Console.WriteLine("instance created..");
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = null;
+            Singleton s2 = null;
+          
+
+
+            Thread t1 = new Thread(() =>
+            {
+                 s1 = Singleton.GetSingleton;
+                 
+            });
+            Thread t2 = new Thread(() =>
+            {
+                 s2 = Singleton.GetSingleton;
+            });
+            /*Singleton s1 = Singleton.GetSingleton();
+            Singleton s2 = Singleton.GetSingleton();
+            
+            */
+            t1.Start();
+            t2.Start();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton Work ..");
+            }
+            else
+            {
+                Console.WriteLine("Singleton doesn't work..");
+            }
+
+
+
+        }
+    }
+}
