ceab6169f7cae9efe209f65271322753e23085a6
shahad <shahad@test>
2021-06-03 01:56:56 +0300

    First Commit

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Singleton
+{
+
+       public class singleton
+    {
+        private static singleton instance;
+        private singleton() { }
+        public static singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new singleton();
+            }
+            return instance;
+        }
+    
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        
+            singleton instance1 = singleton.GetInstance();
+            singleton instance2 = singleton.GetInstance();
+
+            Console.WriteLine(instance1);
+            Console.WriteLine(instance2);
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine(" same instance");
+            }
+            else
+            {
+                Console.WriteLine(" NOT same instance");
+
+            }
+
+        }
+    }
+
+}
