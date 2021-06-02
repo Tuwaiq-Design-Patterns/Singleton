bd2437140aba13a1ef64021da005fbd048b58b58
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-06-02 22:02:21 +0300

    add: Projects Files

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton s = Singleton.Create();
+            Singleton d = Singleton.Create();
+            WriteLine(d == s);
+        }
+    }
+    public class Singleton
+    {
+        private static Singleton _singleton;
+        private Singleton() { }
+        public static Singleton Create() => _singleton ??= new Singleton();
+    }
+}
