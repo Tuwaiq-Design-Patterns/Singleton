c556f878a8d94c714a09c0a1bb7f7453132231b7
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-06-02 15:44:52 +0300

    Added Project files

---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        public class Singleton
+        {
+            private static Singleton instance;
+
+            private Singleton()
+            {
+              
+            }
+
+            public static Singleton getInstance()
+            {
+                if (instance != null)
+                {
+                    Console.WriteLine("Returning Instance");
+                    return instance;
+                } 
+                else
+                {
+                    Console.WriteLine("Creating new instance");
+                    instance = new Singleton();
+                    return instance;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Singleton singleton = Singleton.getInstance();
+            Singleton singleton2 = Singleton.getInstance();
+        }
+    }
+}
