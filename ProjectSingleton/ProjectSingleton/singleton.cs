73f624793daf23502d99634ec75650f44d5f8480
Ahmed <dev@test.com>
2021-06-03 16:10:46 +0300

    Done

---
+++ ProjectSingleton/ProjectSingleton/singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ProjectSingleton
+{
+    public class Singleton
+    {
+        static Singleton object_Singleton;
+        public string Value { get; set; }
+        private Singleton()
+        {
+
+        }
+
+
+        public static Singleton GetSingleton()
+        {
+
+            if (object_Singleton ==null)
+            {
+                Console.WriteLine("Create New Singleton object");
+                object_Singleton = new Singleton();
+            }
+                return object_Singleton;
+        }
+    }
+}
