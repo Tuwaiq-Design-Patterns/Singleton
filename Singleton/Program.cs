51ee6315a42bb8685abe6734373563209490384f
SamirahAlhusayni <samirah.alhusayni@tuwaiq.edu.sa>
2021-06-03 16:26:23 +0300

    singleton uploaded

---
+++ Singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object _lock = new object();
+        private Singleton()
+        {
+        }
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = null;
+            Singleton s2 = null;
+            //thread save singleton
+            Thread th1 = new Thread(() => {
+                s1 = Singleton.getInstance();
+            });
+            Thread th2 = new Thread(() => {
+                s2 = Singleton.getInstance();
+            });
+            //Check if the object is created again or it’s same
+            if (s1 == s2)
+                Console.WriteLine("sami");
+            else
+                Console.WriteLine("not");
+        }
+    }
+}
