44178917bcc7ee78645a7b6f5a14bce9ba1db7af
AfraAllahyani <Afra.Allahyani@tuwaiq.edu.sa>
2021-06-03 15:43:50 +0300

    singleton HW

---
+++ singleton_exc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace singleton_exc
+{
+    public class SingleTon
+    {
+        private static SingleTon obj;
+        private SingleTon() { }
+        public static SingleTon getInstance() {
+            if (obj == null)
+            {
+                obj = new SingleTon();
+            }
+            return obj;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTon s1= SingleTon.getInstance();
+            SingleTon s2 = SingleTon.getInstance();
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            if (s1 == s2)
+            {
+                Console.WriteLine(" objaects are same .");
+            }
+            else
+            {
+                Console.WriteLine(" objaects are (NOT) same .");
+
+            }
+        }
+    }
+}
