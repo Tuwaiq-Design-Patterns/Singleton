ba3c48bfdf423ec600f314b51e3b01c58f1dc372
Faisal Abdulaziz <faisal.alsagri.1@gmail.com>
2021-06-03 16:56:47 +0300

    init

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2) Console.WriteLine("objaects are the same.");
+            else Console.WriteLine("objaects NOT the same .");
+        }
+    }
+    class Singleton
+    {
+        private Singleton() { }
+        private static Singleton _instance;
+        public static Singleton GetInstance() => _instance ??= new Singleton();
+
+    }
+}
