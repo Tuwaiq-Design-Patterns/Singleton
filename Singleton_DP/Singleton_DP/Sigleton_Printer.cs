b423014d62e8e264fec0f888e5a12f5fd2df32bd
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-06-03 09:47:42 +0300

    initial commit

---
+++ Singleton_DP/Singleton_DP/Sigleton_Printer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton_DP
+{
+    class Printer
+    {
+        private static Printer _instance = null;
+        private Printer()
+        {
+            Console.WriteLine("Singleton Initilized");
+        }
+        public static Printer getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Printer();
+            }
+
+            return _instance;
+        }
+
+        public void print_papers()
+        {
+            Console.WriteLine("Print Papers");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer singleton = Printer.getInstance();
+            Printer singleton2 = Printer.getInstance();
+
+            //Printer singleton2
+        }
+    }
+}
