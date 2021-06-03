5e7848bf670f33f68d20e84d12d3e2a4cd4ee97d
Turki A <taj@Turkis-MacBook-Air.local>
2021-06-03 09:19:51 +0300

    Add: singleton design pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+        private static Singleton instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.Instance;
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
