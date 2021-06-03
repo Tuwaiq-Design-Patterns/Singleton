03fe4f627ff8b0297860c1c899f2825f0713f8af
HassanAlgadah <h--san@hotmail.com>
2021-06-03 16:09:19 +0300

    first

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton k = null;
+            Singleton j = null;
+            Thread th1 = new Thread(() =>
+            {
+                k = Singleton.getInctence();
+            });
+            Thread th2 = new Thread(() =>
+            {
+                j = Singleton.getInctence();
+            });
+            th1.Start();
+            th2.Start();
+            Console.WriteLine(k == j);
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object _lock = new object();
+
+        private Singleton(){}
+
+        public static Singleton getInctence()
+        {
+            
+            
+            if (instance == null)
+            {
+                lock (_lock){
+                    if (instance == null)
+                    {
+                        return instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+            
+        }
+    }
+}
