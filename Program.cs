6d06593ec3b40715487d33e0842c6745bde40751
maryam-alraddadi <maryam.alraddadi@tuwaiq.edu.sa>
2021-06-03 03:32:38 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Singleton
+{
+
+    class Singleton
+    {
+        private static Singleton _singleton;
+
+        public Singleton()
+        {
+        }
+
+         public static Singleton getInstance()
+         {
+             return _singleton ?? new Singleton();
+         }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+            Console.WriteLine(s.Equals(s2));
+            Console.WriteLine(s.GetHashCode());
+            Console.WriteLine(s2.GetHashCode());
+        }
+    }
+}
