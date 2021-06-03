4894e08dfb2215dd21c710c4f408d13bf141ad8d
meshal <meshal.najdi@tuwaig.edu.sa>
2021-06-03 16:26:31 +0300

    1st

---
+++ Singlton first Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singlton_first_Example
+{
+    public class singlton
+    {
+        private static singlton ins;
+        private string f;
+        private singlton() { }
+        public static singlton getIns() 
+        {
+            if (ins == null)
+            {
+                ins = new singlton();
+                ins.f = "this is printable";
+
+            }
+
+            return ins;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            singlton r= singlton.getIns();
+            singlton x = singlton.getIns();
+
+            if (r==x)
+            {
+                Console.WriteLine("true");
+
+            }
+        }
+    }
+    
+}
