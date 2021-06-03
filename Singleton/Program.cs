525dd862bcb96e23ad62c14460b033ed91a3f43f
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-06-03 13:24:27 +0300

    Singleton HW

---
+++ Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Singleton
+{
+    
+    class Singleton
+    {
+       
+        private Singleton() { }
+
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public static void someBusinessLogic()
+        {
+            Console.WriteLine("Magic");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Singleton sing1 = Singleton.GetInstance();
+            Singleton sing2 = Singleton.GetInstance();
+
+            if (sing1 == sing2)
+            {
+                Console.WriteLine("Singleton works!");
+            }
+            else
+            {
+                Console.WriteLine("Singleton doesn't work..");
+            }
+        }
+    }
+}
