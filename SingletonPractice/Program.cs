4438c0f601d047323fe211925db08a3df70c1b39
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-03 16:18:22 +0300

    Singleton Practice

---
+++ SingletonPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace SingletonPractice
+{
+
+    class Singleton
+    {
+
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+
+        }
+
+
+        public static Singleton getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+                return _instance;
+            }
+            else
+            {
+                return _instance;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton test1 = Singleton.getInstance();
+            Singleton test2 = Singleton.getInstance();
+            
+
+            if (test1 == test2)
+            {
+                Console.WriteLine("THEY ARE THE SAME.");
+            }
+            else
+            {
+                Console.WriteLine("THEY ARE **NOT** THE SAME.");
+            }
+
+        }
+    }
+}
