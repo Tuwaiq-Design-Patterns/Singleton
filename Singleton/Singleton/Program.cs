d5548618dcda116f8256196f236d5d341769d328
Reema3Alyousef <reema.alyousef@tuwaiq.edu.sa>
2021-06-03 09:16:18 +0300

    submitted project

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+namespace Singleton
+{  
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                singleton instance1 = null;
+                singleton instance2 = null;
+
+
+                //thread save singleton
+                Thread th1 = new Thread(() =>
+                {
+
+                    instance1 = singleton.getInstance();
+                });
+
+
+                Thread th2 = new Thread(() =>
+                {
+
+                    instance2 = singleton.getInstance();
+                });
+
+
+                if (instance1 == instance2)
+                    Console.WriteLine("Same objects");
+                else
+                    Console.WriteLine("Different objects");
+
+
+            }
+        }
+    }
+
+public class singleton
+{
+    private static singleton instance;
+    private singleton() { }
+    public static singleton getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new singleton();
+        }
+        return instance;
+    }
+}
