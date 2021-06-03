a059d7eee65e1cf999b26bfe8a8ce09648430474
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-06-03 10:42:59 +0300

    Add Files

---
+++ Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton one = Singleton.GetInstance();
+            Singleton two = Singleton.GetInstance();
+
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton Instance;
+        private Singleton() 
+        {
+            Console.WriteLine("Instance Created");
+        }
+        public static Singleton GetInstance()
+        {
+            if(Instance == null)
+            {
+                Instance = new Singleton();
+            }
+
+            return Instance;
+        }
+    }
+}
