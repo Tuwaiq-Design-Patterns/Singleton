28f2e1a5fdbd9b9746c39ac67f772d0a480ad17d
Arwa wanla <arwawanla@Arwas-MacBook-Pro-4.local>
2021-06-08 00:02:13 +0300

    Added: Practice

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Singleton
 this repo contains singleton design pattern implementation using C#
+
+### Trainee Name:
+Arwa Wan La
+
+### Group:
+Direyah
+
+### Instructor:
+Hatim AlSalman
---
+++ singletonPractice/singleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace singleton
+{
+    class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            if (instance ==null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+            
+        }
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton s = Singleton.GetInstance();
+
+            Console.WriteLine("Singleton " + s);
+
+            Singleton s2 = Singleton.GetInstance();
+            Console.WriteLine("Singleton " + s2);
+
+            if (s == s2)
+            {
+                Console.WriteLine("instances are the same");
+
+            }
+            else
+            {
+                Console.WriteLine("they are differenct");
+            }
+        }
+    }
+}
